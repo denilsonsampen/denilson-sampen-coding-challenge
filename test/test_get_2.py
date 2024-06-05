ed8e25bd6a06d8740596e2b6f188d46da6fc30c2
Denilson Sampen <denilson.sampen@applying.cloud>
2024-06-05 11:29:28 -0500

    update: README.md

--- test/test_get_2.py
+++ test/test_get_2.py
@@ -1,12 +1,16 @@
 import requests
 
-#url = 'http://127.0.0.1:5000/employees_by_job_department_2021'
+# Define the URL for the API endpoint
 url = 'http://127.0.0.1:5000/departments_above_average'
 
+# Make a GET request to the API endpoint
 response = requests.get(url)
 
+# Check if the request was successful
 if response.status_code == 200:
+    # Parse the JSON response and print it
     data = response.json()
     print(data)
 else:
+    # Print an error message if the request failed
     print('Error:', response.status_code)
--- test/test_post.py
+++ test/test_post.py
@@ -1,9 +1,16 @@
 import requests
 
+# Define the URL for the API endpoint
 url = 'http://127.0.0.1:5000/upload_csv'
-files = {'file': open('./data/jobs.csv', 'rb')}
-data = {'table': 'jobs'}
+
+# Open the CSV file
+files = {'file': open('./data/jobs.csv', 'rb')} # This can be change from departments, jobs and hired_employees
+
+# Define the form data to specify table name
+data = {'table': 'jobs'}    # Has to match CSV file name
+
+# Make a POST request to the API endpoint with the CSV file and form data
 response = requests.post(url, files=files, data=data)
 
-
+# Print the response text
 print(response.text)
