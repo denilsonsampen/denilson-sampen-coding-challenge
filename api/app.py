ffb29deda1f80a0c17dcea3f44e5ab546580517c
Denilson Sampen <denilson.sampen@applying.cloud>
2024-06-05 01:19:06 -0500

    update: Comments

--- api/app.py
+++ api/app.py
@@ -11,22 +11,27 @@ DB_URL = 'sqlite:///../db.globant_db'
 engine = create_engine(DB_URL)
 Session = sessionmaker(bind=engine)
 
+# Maximum number of rows to process per batch
 MAX_BATCH_SIZE = 1000
 
+# Define API POST method
 @app.route('/upload_csv', methods=['POST'])
 def upload_csv():
     try:
+        # Read CSV file from request
         file = request.files['file']
         if not file:
             return jsonify({'error': 'No file provided'}), 400
         
+        # Read CSV to Dataframe
         data = read_csv(file)
         table_name = request.form['table']
 
+        # Insert data per batch
         batch = []
-
         with Session() as session:
             for index, row in data.iterrows():
+                # Create model instance by table name
                 if table_name == 'departments':      
                     model = Departments(id=row[0], department=row[1])
                 elif table_name == 'jobs':
@@ -36,12 +41,15 @@ def upload_csv():
                 else:
                     return jsonify({'error': 'Invalid table name'}), 400
                 
+                # Append model to batch
                 batch.append(model)
 
+                # Insert batch if size exceeds MAX_BATCH_SIZE
                 if len(batch) >= MAX_BATCH_SIZE:
                     insert_batch(session, model.__class__, batch)
                     batch = []
             
+            # Insert remaining batch
             if batch:
                 insert_batch(session, model.__class__, batch)
         
